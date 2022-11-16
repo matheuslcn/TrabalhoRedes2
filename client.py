edbe29bf7253ef12043ed8d3fbe43f7694e4b610
Matheus Lopes Carapina do Nascimento <71265929+matheuslcn@users.noreply.github.com>
2022-11-16 15:02:42 -0300

    criar soquetes udp para o cliente falar e escutar

--- client.py
+++ client.py
@@ -1,12 +1,19 @@
 import socket
-import multiprocessing
+import threading
 
 class Client:
-    def __init__(self, host, port):
+    def __init__(self, host, tcp_port, udp_listen, udp_speak):
         self.host = host
-        self.port = port
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.connect((self.host, self.port))
+        self.tcp_port = tcp_port
+        self.tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.tcp_sock.connect((self.host, self.tcp_port))
+        self.udp_listen_port = udp_listen
+        self.udp_listen_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.udp_listen_sock.bind((self.host, self.udp_listen_port))
+        self.udp_speak_port = udp_speak
+        self.udp_speak_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.udp_speak_sock.connect((self.host, self.tcp_port))
+
 
     def send(self, msg):
         self.sock.send(msg.encode())
@@ -16,6 +23,25 @@ class Client:
 
     def close(self):
         self.sock.close()
+
+    def call(self):
+        thread_listen = threading.Thread(target=self.listen_call)
+        thread_listen.start()
+        thread_listen = threading.Thread(target=self.speak_call)
+        thread_listen.start()
+
+        
+    def listen_call(self):
+        while True:
+            msg = self.udp_listen_sock.recv(1024).decode()
+            print(msg)
+    
+    def speak_call(self):
+        while True:
+            msg = input()
+            self.udp_speak_sock.send(msg.encode())
+
+
 
 if __name__ == '__main__':
     client = Client('localhost', 5000)
