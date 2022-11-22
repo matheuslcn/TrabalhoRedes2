01eadf935b0ccaf01bd393e44af1c490fbac324e
Matheus Lopes Carapina do Nascimento <71265929+matheuslcn@users.noreply.github.com>
2022-11-22 20:39:49 -0300

    resolvendo conflito

--- USER/client.py
+++ USER/client.py
@@ -3,7 +3,6 @@ import threading
 import pyaudio
 import pickle
 
-from callServer import *
 from tkinter import messagebox
 
 FRAMES_PER_BUFFER = 1024*2
@@ -12,7 +11,7 @@ CHANNELS = 1
 RATE = 16000
 
 class Client:
-    def __init__(self, host, tcp_port, udp_listen_port, udp_speak_port):
+    def __init__(self, host, tcp_port, udp_port, call_port):
         self.username = self.change_username()
         self.host = host
 
@@ -20,25 +19,23 @@ class Client:
         self.tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.tcp_sock.connect((self.host, self.tcp_port))
 
-        self.udp_call_port = udp_speak_port
-        self.udp_call_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.udp_call_sock.bind((self.host, self.udp_call_port))
+        self.udp_port = udp_port
+        self.udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.udp_sock.bind((self.host, self.udp_port))
 
-        self.call_server = CallServer(self.host, udp_listen_port, self.udp_call_port)
-        threaded_server = threading.Thread(target=self.call_server.start)
-        threaded_server.daemon = True
-        threaded_server.start()
+        self.call_port = call_port
 
         self.py_audio = pyaudio.PyAudio()
 
         self.is_logged = False
+        self.in_call = False
 
     def change_username(self):
         username = input("Digite seu nome de usuario: ")
         return username
 
     def start(self):
-        self.tcp_send(f'login {self.username} {self.call_server.port}')
+        self.tcp_send(f'login {self.username} {self.udp_port}')
         thread_receive_message = threading.Thread(target=self.tcp_receive)
         thread_receive_message.daemon = True
         thread_receive_message.start()
@@ -56,7 +53,6 @@ class Client:
                 'resposta_login': self.login,
                 'resposta_consulta': self.invite,
                 'resposta_convite': self.call,
-
             }
             commands[msg_list[0]](msg_list)
                         
@@ -68,7 +64,7 @@ class Client:
         elif action == 'usuario_existente':
             print("Usuario ja existe")
             self.username = self.change_username()
-            self.tcp_send(f'login {self.username} {self.call_server.port}')
+            self.tcp_send(f'login {self.username} {self.udp_port}')
 
     def get_user_information(self):
         username = input("Digite o nome do usuario que deseja chamar: ")
@@ -77,8 +73,6 @@ class Client:
     def close(self):
         self.tcp_send('logout')
         self.tcp_sock.close()
-        self.call_server.close()
-        self.udp_call_sock.close()
         self.is_logged = False
         
     def invite(self, msg_list):
@@ -88,80 +82,88 @@ class Client:
         else:
             ip = msg_list[2]
             port = int(msg_list[3])
-            self.udp_send(f"convite {self.username} {self.host} {self.udp_call_port}", ip, port)
+            self.udp_send(f"convite {self.username} {self.call_port}", ip, port)
             print(f"Convite enviado para {username}")     
         
-    def listen(self):
+    def listen(self, sock):
         stream = self.py_audio.open(format=FORMAT,
                         channels=CHANNELS,
                         rate=RATE,
                         output=True,
                         frames_per_buffer=FRAMES_PER_BUFFER)
-        while self.call_server.in_call:
+        while self.in_call:
             try:
-                audio, _ = self.udp_call_sock.recvfrom(1024 * 8)
+                audio, _ = sock.recvfrom(1024 * 8)
                 p_audio = pickle.loads(audio)
+                if p_audio == b'fim_chamada':
+                    self.in_call = False
+                    break
                 stream.write(p_audio)
             except:
                 pass
         
-    def speak(self, ip, port):
+    def speak(self, sock, ip, port):
         stream = self.py_audio.open(format=FORMAT,
                         channels=CHANNELS,
                         rate=RATE,
                         input=True,
                         frames_per_buffer=FRAMES_PER_BUFFER)
-        while self.call_server.in_call:
+        while self.in_call:
             audio = stream.read(FRAMES_PER_BUFFER)
             p_audio = pickle.dumps(audio)
-            if not self.call_server.in_call:
+            if not self.in_call:
                 break
-            self.udp_call_sock.sendto(p_audio, (ip, port))
-    
-    def end_call(self):
-        self.call_server.in_call = False
-        self.udp_call_sock.close()
-        self.udp_call_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.udp_call_sock.bind((self.host, self.udp_call_port))
-    
+            sock.sendto(p_audio, (ip, port))
+
+    def end_call_popup(self, sock, ip, port):
+        messagebox.showinfo(f"Fim da chamada do {self.username}", "Deseja finalizar a chamada?")
+        self.in_call = False
+        sock.sendto(b'fim_chamada', (ip, port))
+
+    def end_call(self, *_):
+        self.in_call = False
+
     def udp_send(self, msg, ip, port):
-        self.udp_call_sock.sendto(msg.encode(), (ip, port))
+        self.udp_sock.sendto(msg.encode(), (ip, port))
 
     def udp_receive(self):
-        msg, (ip_call_server, port_call_server) = self.udp_call_sock.recvfrom(1024)
+        msg, (ip, port) = self.udp_sock.recvfrom(1024*8)
         print(msg)
         msg_list = msg.decode().split()
         commands = {
-            'convite': self.receive_invite,
-            'resposta_convite': self.call,
-            'mensagem_invalida': lambda *_: None,
+        'convite': self.receive_invite,
+        'resposta_convite': self.call,
+        'mensagem_invalida': lambda *_: None,
         }
-        commands.get(msg_list[0], self.udp_message_error)(msg_list, (ip_call_server, port_call_server))
+        commands.get(msg_list[0], self.udp_message_error)(msg_list, (ip, port))
+        
 
     def receive_invite(self, msg_list, ip_port):
-        ip_call_server, port_call_server = ip_port
+        ip, port = ip_port
+        port = int(port)
         username = msg_list[1]
-        ip = msg_list[2]
-        port = int(msg_list[3])
-        
+        call_port = int(msg_list[2])
+        if self.in_call:
+            self.udp_send(f'resposta_convite ocupado', ip, port)
+            return
         option = messagebox.askyesno(f"Ligação para {self.username}", f"Voce deseja atender a ligação de {username}?")
         if option:
-            self.call_server.in_call = True
-            self.start_call(ip, port)
+            self.in_call = True
+            self.start_call(ip, call_port)
             ans = "disponivel"
         else:
             ans = "recusou"
-        self.udp_send(f"resposta_convite {ans} {ip} {port}", ip_call_server, port_call_server)
+        self.udp_send(f"resposta_convite {ans} {self.call_port}", ip, port)
 
-    def call(self, msg_list, _):
+    def call(self, msg_list, ip_port):
+        ip, _ = ip_port
         status = msg_list[1]
+        call_port = int(msg_list[2])
         if status == "ocupado" or status == "recusou":
             print(f"Usuario {status}")
-            self.call_server.in_call = False
+            self.in_call = False
         elif status == "disponivel":
-            ip = msg_list[2]
-            port = int(msg_list[3])
-            self.start_call(ip, port)
+            self.start_call(ip, call_port)
 
     def tcp_message_error(self, *_):
         self.tcp_send("mensagem_invalida")
@@ -171,20 +173,25 @@ class Client:
 
 
     def start_call(self, ip, port):
-        thread_speak = threading.Thread(target=self.speak, args=(ip, port))
-        thread_speak.start()  
-        thread_listen = threading.Thread(target=self.listen)
+        self.in_call = True
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.bind((self.host, self.call_port))
+        thread_speak = threading.Thread(target=self.speak, args=(sock, ip, port))
+        thread_speak.start()
+        thread_listen = threading.Thread(target=self.listen, args=(sock,))
         thread_listen.start()
+        thread_end_call = threading.Thread(target=self.end_call_popup, args=(sock, ip, port))
+        thread_end_call.start()
         
     def menu(self):
         print("1 - Chamar usuario")
         print("2 - Sair")
         print("Digite a opcao desejada: ")
-        while not self.call_server.in_call:
+        while not self.in_call:
             option = input('')
             if option == '1':
-                if not self.call_server.in_call:
-                    self.call_server.in_call = True
+                if not self.in_call:
+                    self.in_call = True
                     self.get_user_information()
                 else:
                     print("Voce ja esta em uma chamada")
@@ -196,15 +203,15 @@ class Client:
 
 if __name__ == '__main__':
     udp_listen = int(input("Digite a porta UDP para receber mensagens: "))
-    udp_speak = int(input("Digite a porta UDP para enviar mensagens: "))
-    client = Client('localhost', 5000, udp_listen, udp_speak)
+    call_port = int(input("Digite a porta UDP para receber chamadas: "))
+    client = Client('localhost', 5000, udp_listen, call_port)
     client.start()
 
     while not client.is_logged:
         pass
 
     while client.is_logged:
-        while client.call_server.in_call:
+        while client.in_call:
             pass
         
         t_menu = threading.Thread(target=client.menu)
@@ -214,7 +221,5 @@ if __name__ == '__main__':
         t_receive_invite.daemon = True
         t_receive_invite.start()
 
-
-        while not client.call_server.in_call:
+        while not client.in_call:
             pass
-
